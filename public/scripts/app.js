4e0a791d27a86832c176d103ba1c0f64729fa2b9
fchow2018 <dong_chow@yahoo.com>
2018-04-22 17:52:05 -0700

    Started add song lyrics page

---
+++ public/scripts/app.js
@@ -0,0 +1,30 @@
+$("#btn_signup").on('click',function(){
+
+  var data=$('#signup form').serialize();
+  $.ajax({
+    url:'/signup',
+        method:'post',
+      data:data,
+    success:function(){window.location.href='/home'},
+    error:function(json){
+      $('.modal').modal('hide');
+      console.log(json);
+      $('#error').text(json.responseJSON.message.message);
+    }
+  });
+});
+$("#btn_login").on('click',function(){
+
+  var data=$('#login form').serialize();
+  $.ajax({
+    url:'/login',
+        method:'post',
+      data:data,
+    success:function(){window.location.href='/home'},
+    error:function(json){
+      $('.modal').modal('hide');
+      console.log(json);
+      $('#error').text('wrong email and password');
+    }
+  });
+});
--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 var express=require('express');
 var app=express();
 var db=require('./models/index.js');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'),
+methodOverride = require('method-override'),
+cookieParser = require('cookie-parser'),
+session = require('express-session'),
+passport = require('passport'),
+LocalStrategy = require('passport-local').Strategy;
+
+// Require Lyrics Post model
+var db = require("./models"),
+lyricsPost = db.lyricsPost,
+User = db.User;
 
 // Configure app
 app.set('views', __dirname + '/views');      // Views directory
 app.use(express.static('public'));          // Static directory
 app.use(bodyParser.urlencoded({ extended: true })); // req.body
+
+app.use(methodOverride("_method"));
+
+app.use(cookieParser());
+app.use(session({
+  secret: "thisisasecret", // change this!
+  resave: false,
+  saveUninitialized: false,
+}));
+// app.use(passport.initialize());
+// app.use(passport.session());
+//
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
 // Set CORS Headers
 app.use(function(req, res, next) {
@@ -17,7 +42,24 @@ app.use(function(req, res, next) {
 
 //basic root route
 app.get('/',function(req,res){
-  res.render('home');
+    res.render('home');
+  });
+
+app.get('/dashboard',function(req,res){
+    res.render('dashboard');
+  });
+
+app.get('/add_song',function(req,res){
+    res.render('dashboard');
+  });
+
+  app.post('/home_user', passport.authenticate('local'),function (req,res) {
+  if(!req.user){
+    res.status(400);
+  }else{
+
+    res.render('/home');
+  }
   });
 
 app.set("view engine", "ejs");
